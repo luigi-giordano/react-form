87bbd770b4e25852368ac6f415324c6459811dfb
luigi-giordano <luigi.giordano1992@gmail.com>
2025-01-19 15:17:10 +0100

    useState per inserire nome nel campo imput del form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import React, { useState } from 'react';
+
 const FormBase = () => {
 
   const [firstName, setFirstName] = useState('');
 
   return (
     <div className="container my-5">
-      <div className="title my-5"><h2>Blog</h2></div>
+      <div className="title my-5"><h2>Inserimento Nomi: {firstName}</h2></div>
       <div className="row">
         <div className="col-6">
           <form action="#">
             <input
               className="form-control"
               type="text"
-              placeholder="Inserisci Nome" />
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="Inserisci Nome"
+            />
             <button
               className="btn btn-primary my-5 w-100"
             >Invio</button>
